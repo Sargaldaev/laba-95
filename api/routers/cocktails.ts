33fc6810f187ab5e518eadfa63bb2b81450fe552
elaman sargaldaev <melisbekovelaman311@gmail.com>
2024-03-26 18:36:53 +0600

    реализовал удаление и изменение состояние публикации так же гет запрос

--- api/routers/cocktails.ts
+++ api/routers/cocktails.ts
@@ -3,11 +3,12 @@ import { Error } from 'mongoose';
 import { imagesUpload } from '../multer';
 import Cocktail from '../models/Cocktail';
 import auth, { RequestWithUser } from '../middleware/auth';
+import permit from '../middleware/permit';
 
 const cocktailsRouter = express.Router();
 
 
-cocktailsRouter.post('/',auth, imagesUpload.single('image'), async (req, res, next) => {
+cocktailsRouter.post('/', auth, imagesUpload.single('image'), async (req, res, next) => {
   const user = (req as RequestWithUser).user;
 
   try {
@@ -27,6 +28,63 @@ cocktailsRouter.post('/',auth, imagesUpload.single('image'), async (req, res, ne
       return res.status(422).send(error);
     }
     return next(error);
+  }
+});
+
+
+cocktailsRouter.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const cocktail = await Cocktail.findById(id).populate('user', 'displayName');
+    return res.send(cocktail);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+cocktailsRouter.get('/', async (req, res) => {
+  try {
+    const cocktails = await Cocktail.find();
+    return res.send(cocktails);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+
+cocktailsRouter.delete('/:id', auth, permit('admin'), async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const cocktail = await Cocktail.findByIdAndDelete(_id);
+    if (!cocktail) {
+      return res.status(404).send({message: 'Cocktail not found'});
+    }
+    return res.send({message: 'Cocktail deleted'});
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+
+cocktailsRouter.patch('/:id/togglePublished', auth, permit('admin'), async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const cocktail = await Cocktail.findOne({ _id });
+
+    if (!cocktail) {
+      return res.status(404).send({ message: 'Cocktail not found' });
+    }
+    if (cocktail.isPublished === false) {
+      await Cocktail.findOneAndUpdate({ _id }, { isPublished: true });
+      return res.send({ message: 'Cocktail updated' });
+    } else {
+      await Cocktail.findOneAndUpdate({ _id }, { isPublished: false });
+      return res.send({ message: 'Cocktail updated' });
+    }
+  } catch (e) {
+    return res.send(e);
   }
 });
 
